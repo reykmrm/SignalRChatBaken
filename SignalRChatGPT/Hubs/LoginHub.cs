1fabd9d8708060832804524e2fab698cab6d0433
ElSebastianLopez <sebaslopezhenao@gmail.com>
2023-05-19 20:20:32 -0500

    :v

--- SignalRChatGPT/Hubs/LoginHub.cs
+++ SignalRChatGPT/Hubs/LoginHub.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRChatGPT.Modelos;
+using SignalRChatGPT.Modelos.DTOs;
+using SignalRChatGPT.Services;
 
 namespace SignalRChatGPT.Hubs
 {
-    public class LoginHub : Controller
+    public class LoginHub : Hub
     {
-        public IActionResult Index()
+        private readonly UserService _userService;
+        public LoginHub(UserService userService)
         {
-            return View();
+            _userService = userService;
+        }
+        public async Task Login(UsuariosDTO user)
+        {
+            Usuario userByLogin = await _userService.GetUserByLogin(user.Usuario, user.Clave);
+            if (userByLogin != null) 
+            {
+
+            }
         }
     }
 }
