7e075c376d415d9bd21138107ac2e1a04837c422
Reyk Mrm <elpantera142@gmail.com>
2023-05-27 00:26:18 -0500

    chat funcionando

--- SignalRChatGPT/Hubs/ChatHub.cs
+++ SignalRChatGPT/Hubs/ChatHub.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace SignalRChatGPT.Hubs
 {
-    public class ChatHub: Hub
+    public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string iduser, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+            await Clients.Client(iduser).SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task SendMessageToGroup(string groupName, string message)
+        {
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", message);
         }
     }
 }
